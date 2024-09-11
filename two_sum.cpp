88d7f1957aef9ca665652227e64587fd2566351f
1mmhg <eaglechen2020@gmail.com>
2024-09-11 21:55:10 +0800

    nothing

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,16 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> numMap;//{number, index}
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> numMap; //{number, index}
         int n = nums.size();
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++)
+        {
             int search_target = target - nums[i];
-            if(numMap.count(search_target)){
+            if (numMap.count(search_target))
+            {
                 return {numMap[search_target], i};
             }
             numMap[nums[i]] = i;
         }
+        return {};
         return {};
     }
 };
